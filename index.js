4bef7a2f5fcf6a192df4b3ba240abb93f9fe98b3
Anjita Gargi Chandora <91597725+anjitagargi@users.noreply.github.com>
2022-11-20 11:54:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,116 @@
+for(var i=0; i< document.querySelectorAll(".drum").length; i++ ){
+
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+   var buttonInnerHTML = this.innerHTML;
+   makeSound(buttonInnerHTML);
+   buttonAnimation(buttonInnerHTML);
+});
+}
+
+    
+//detecting keyboard press
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+
+function makeSound(key){
+    switch (key) {
+
+        case "w":
+            var tom1  =new Audio('sounds/tom-1.mp3');
+       tom1.play();
+         break;
+    
+         case "a":
+            var tom2  =new Audio('sounds/tom-2.mp3');
+       tom2.play();
+         break;
+    
+         case "s":
+            var tom3  =new Audio('sounds/tom-3.mp3');
+       tom3.play();
+         break;
+    
+         case "d":
+            var tom4  =new Audio('sounds/tom-4.mp3');
+       tom4.play();
+         break;
+    
+         case "j":
+            var snare  =new Audio('sounds/snare.mp3');
+        snare.play();
+         break;
+    
+         case "k":
+            var  crash =new Audio('sounds/crash.mp3');
+        crash.play();
+         break;
+         
+         case "l":
+            var kick =new Audio('sounds/kick-bass.mp3');
+        kick.play();
+         break;
+       
+       
+        default: 
+        console.log(buttonInnerHTML);
+       }
+
+}
+
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100 );
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// var audio =new Audio('sounds/tom-1.mp3');
+//    audio.play();
+
+//  function housekeeper(yearsofExperience,name,cleaningRe){
+//     this.yearsofExperience = yearsofExperience;
+//     this.name = name;
+//     this.cleaningRe= cleaningRe;
+//  }
+ 
+//  var housekeeper1= new housekeeper(12,"jane",["bathroom","lobby"]);
+  
+
+// function anotherAddEventListner(typeOfEvent, callback){
+//     //detect event code
+//     var eventThatHappened = {
+//         eventType : "keypress",
+//         key : "p",
+//         durationOfKeyPress:2
+//     }
+//     if(eventThatHappened.eventType== typeOfEvent){
+//         callback(eventThatHappened);
+//     }
+// }
+
+// anotherAddEventListner("keypress",function(event){
+//     console.log(event);
+// });
+
+// debugger;
+// document.anotherAddEventListner("keypress",function(event){
+//     console.log(event);
+// });
